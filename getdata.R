28964d0cff1a543374e8fe59a3e7db4658465367
Hao Zhu <haozhu233@gmail.com>
2019-04-18 15:37:49 -0400

    initial commit

---
+++ getdata.R
@@ -0,0 +1,4 @@
+install.packages("babynames")
+
+download.file("https://gist.githubusercontent.com/stevejackson/1429696/raw/0ae04f6d1af406a43ca617b2bdb9b2ef7034ba5a/gistfile1.txt",
+              "pinyin.txt")
